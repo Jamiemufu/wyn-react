d8fa0594f0f93390139719b56621a16010b1dd11
Jamiemufu <taichi.chikung@gmail.com>
2023-08-29 18:12:54 +0100

    Optimised logo and reduced loading height

--- app/components/nav/navLogo.tsx
+++ app/components/nav/navLogo.tsx
@@ -6,8 +6,8 @@ export default function NavLogo() {
     <div className="flex items-center flex-shrink-0 text-grey mr-6">
       <Link href="/" >
         <Image
-          src={"/wynpng.png"}
-          height={275}
+          src={"/logo.png"}
+          height={91}
           width={275}
           alt="Wyn Edwards Logo"
         />
